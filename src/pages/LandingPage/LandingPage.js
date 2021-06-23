ff66c2edb7eeebe2c687e5586dd407f170e7ceb2
이지호 <cctv1998@naver.com>
2021-06-24 00:42:34 +0900

    modify

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./LandingPageStyle.css";
+
 const LandingPage = () => {
   return (
     <div id="land">
       <div id="header">
         <div id="search-area">
           <div className="logo">
-            <a href="/">CAFE</a>
+            <a href="/">JHCAFE</a>
           </div>
           <div className="search-box">
             <input
@@ -30,13 +31,41 @@ const LandingPage = () => {
       </div>
 
       <div id="cafe-container">
-        <div id="cafe-list">
+        <div className="cafe-list">
+          <div className="cafe-object">
+            <div className="cafe-thumb"></div>
+            <strong>
+              <a href="/cafe">●지호카페● 대한민국 일등 카페 </a>
+            </strong>
+          </div>
+          <div className="cafe-recent">
             <ul>
-                <li>지호카페</li>
-                <li>종현카페</li>
-                <li>도현카페</li>
-
+              <li>최근에 올라온글</li>
+              <li>최근에 올라온글</li>
+              <li>최근에 올라온글</li>
             </ul>
+          </div>
+        </div>
+        <div className="cafe-list">
+          <div className="cafe-object">
+            <div className="cafe-thumb"></div>
+            <strong>
+              <a href="/cafe">
+                도현카페-이도현의 만점시험지를 휘날리자★수능 공부 과외 편입 유학
+              </a>
+            </strong>
+          </div>
+        </div>
+        <div className="cafe-list">
+          <div className="cafe-object">
+            <div className="cafe-thumb"></div>
+            <strong>
+              <a href="/cafe">
+                종현카페-전국 대학생
+                모임(대학생,레포트,토익,족보,스펙,공모전,자격증,편입학)
+              </a>
+            </strong>
+          </div>
         </div>
       </div>
     </div>
