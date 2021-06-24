193b4aa3ad4a9c40442c321c266af104362a7f00
이지호 <cctv1998@naver.com>
2021-06-24 14:20:42 +0900

    modify

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -34,9 +34,8 @@ const LandingPage = () => {
         <div className="cafe-list">
           <div className="cafe-object">
             <div className="cafe-thumb"></div>
-            <strong>
-              <a href="/cafe">●지호카페● 대한민국 일등 카페 </a>
-            </strong>
+
+            <a href="/cafe">●지호카페● 대한민국 일등 카페 </a>
           </div>
           <div className="cafe-recent">
             <ul>
@@ -49,22 +48,33 @@ const LandingPage = () => {
         <div className="cafe-list">
           <div className="cafe-object">
             <div className="cafe-thumb"></div>
-            <strong>
-              <a href="/cafe">
-                도현카페-이도현의 만점시험지를 휘날리자★수능 공부 과외 편입 유학
-              </a>
-            </strong>
+            <a href="/cafe">
+              도현카페-수능날 만점시험지를 휘날리자★수능 공부 과외 편입 유학
+            </a>
+          </div>
+          <div className="cafe-recent">
+            <ul>
+              <li>최근에 올라온글</li>
+              <li>최근에 올라온글</li>
+              <li>최근에 올라온글</li>
+            </ul>
           </div>
         </div>
         <div className="cafe-list">
           <div className="cafe-object">
             <div className="cafe-thumb"></div>
-            <strong>
-              <a href="/cafe">
-                종현카페-전국 대학생
-                모임(대학생,레포트,토익,족보,스펙,공모전,자격증,편입학)
-              </a>
-            </strong>
+
+            <a href="/cafe">
+              종현카페-전국 대학생
+              모임(대학생,레포트,토익,족보,스펙,공모전,자격증,편입학)
+            </a>
+          </div>
+          <div className="cafe-recent">
+            <ul>
+              <li>최근에 올라온글</li>
+              <li>최근에 올라온글</li>
+              <li>최근에 올라온글</li>
+            </ul>
           </div>
         </div>
       </div>
