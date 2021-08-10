1bb3379c91e4463bf3071a3df50d1eb27bbb741b
이지호 <cctv1998@naver.com>
2021-08-10 16:21:37 +0900

    Style : RegisterPage

--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -1,9 +1,10 @@
-import axios from "axios";
+import "./style.scss";
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import client from "./../../api/client";
 import { useDispatch, useSelector } from "react-redux";
-import { SetUser } from 'modules/user';
+import { SetUser } from "modules/user";
+import LandingHeader from "layout/LandingHeader";
 
 const Signup = () => {
   const dispatch = useDispatch();
@@ -37,7 +38,7 @@ const Signup = () => {
     };
     try {
       const response = await client.post("/user/register", body);
-      dispatch(SetUser(response.data.user))
+      dispatch(SetUser(response.data.user));
       history.push("/");
     } catch (e) {
       alert("회원가입에 실패했습니다.");
@@ -57,37 +58,50 @@ const Signup = () => {
 
   return (
     <>
-      <div id="Signup">
+      <LandingHeader />
+
+      <div id="Signup-container">
+        <div className="title">회원가입</div>
         <form action="" onSubmit={onSubmit}>
-          <input
-            type="text"
-            value={id}
-            name="id"
-            placeholder="id"
-            onChange={onChange}
-          />
-          <input
-            type="password"
-            value={password}
-            name="password"
-            placeholder="pw"
-            onChange={onChange}
-          />
-          <input
-            type="password"
-            value={passwordCheck}
-            name="passwordCheck"
-            placeholder="pwCheck"
-            onChange={onChange}
-          />
-          <input
-            type="text"
-            value={name}
-            name="name"
-            placeholder="name"
-            onChange={onChange}
-          />
-          <button type="submit">REGISTER</button>
+          <div className="ele-box">
+            <input
+              type="text"
+              value={id}
+              name="id"
+              placeholder="id"
+              onChange={onChange}
+            />
+          </div>
+          <div className="ele-box">
+            <input
+              type="password"
+              value={password}
+              name="password"
+              placeholder="pw"
+              onChange={onChange}
+            />
+          </div>
+          <div className="ele-box">
+            <input
+              type="password"
+              value={passwordCheck}
+              name="passwordCheck"
+              placeholder="pwCheck"
+              onChange={onChange}
+            />
+          </div>
+          <div className="ele-box">
+            <input
+              type="text"
+              value={name}
+              name="name"
+              placeholder="name"
+              onChange={onChange}
+            />
+          </div>
+          <button type="submit" className="login-btn">
+            Register
+          </button>
         </form>
       </div>
     </>
